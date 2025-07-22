b0b8a202f015e1fee0c329691c34a2baa01ff988
Claudio Gomes da Silva Neto <claudiogsn2@gmail.com>
2025-07-22 10:03:41 -0300

    asd

--- api/v1/controllers/UserController.php
+++ api/v1/controllers/UserController.php
@@ -8,7 +8,8 @@ ini_set('memory_limit', '512M');
 
 require_once __DIR__ . '/../database/db.php';
 
-class UserController {
+class UserController
+{
 
     public static function getUserDetails($user)
     {
@@ -53,7 +54,7 @@ class UserController {
     }
 
 
-   public static function getMenuMobile($user_id)
+    public static function getMenuMobile($user_id)
     {
         global $pdo;
 
@@ -98,6 +99,28 @@ class UserController {
 
     }
 
+    public static function getUserRoles($userId)
+    {
+        global $pdo;
 
+        if (empty($userId)) {
+            return ['success' => false, 'message' => 'ID do usuário é obrigatório.'];
+        }
+
+        $sql = "
+            SELECT r.id, r.name, r.custom_code
+            FROM system_user_role ur
+            INNER JOIN system_role r ON ur.system_role_id = r.id
+            WHERE ur.system_user_id = :user_id
+        ";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':user_id' => $userId]);
+
+
+        $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return ['success' => true, 'roles' => $roles];
+    }
 }
 ?>
